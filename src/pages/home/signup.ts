ec6d51e7a2464aadc597a860f8f32446dabba546
Sushant Bilgi <Sushant.Bilgi@extentia05.local>
2017-06-02 17:26:53 +0530

    added two screens with background image

--- src/pages/home/signup.ts
+++ src/pages/home/signup.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-signup',
+  templateUrl: 'signup.html'
 })
 
-export class HomePage {
+export class SignUp {
 
   constructor(public navCtrl: NavController) {
 
