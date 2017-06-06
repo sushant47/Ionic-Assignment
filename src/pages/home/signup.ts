6da7b4f97cdcf48304b59df746a4c97fc9e1e43a
Sushant Bilgi <Sushant.Bilgi@extentia05.local>
2017-06-06 18:58:02 +0530

    added ngmodel

--- src/pages/home/signup.ts
+++ src/pages/home/signup.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Login } from './login';
 
+export class Register {
+  
+    public userName: string;
+    public emailId: string;
+    public password: string;
+    public confirmPassword: string;
+  
+}
+
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
@@ -9,13 +18,14 @@ import { Login } from './login';
 
 export class SignUp {
 
-
+  register:Register = new Register();
   constructor(public navCtrl: NavController) {
 //this.navCtrl.push(Login);
   }
  
 signUp(): void {
-   this.navCtrl.push(Login);
+   alert("" + this.register.userName);
+
   }
 
 }
